65a632be5b6ac47db8e3805285b375520e103466
GabrielAzevedo12 <114695658+GabrielAzevedo12@users.noreply.github.com>
2023-12-07 18:45:16 +0000

    ...

---
+++ src/components/frame_motion/framer_motion.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { motion } from "framer-motion";
+import "./css/styles.css";
+
+export default function FramerMotion() {
+  return (
+    <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} className="motion-teste" />
+  );
+}
+
+/*
+<div className="my-container" style={{
+        height: "50vh",
+        width: "50vw",
+        background: "rgb(61, 112, 112)",
+        position: "relative"
+    }}>
+      <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} />
+    </div>
+*/
