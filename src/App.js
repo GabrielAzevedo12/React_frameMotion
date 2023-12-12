8d70151f06e7fdb61a428db4b98735e35a72fa51
GabrielAzevedo12 Vieira <gazevedo996@gmail.com>
2023-12-12 12:40:18 +0000

    ...

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import './App.css';
 import './propsGerais.css'
-import ButtonToggle from './components/buttonToogle/buttonToggle';
-//import FramerMotion from './components/frame_motion/framer_motion';
 import { FramerMotion5 } from './components/frame_motion/framer_motion5';
 import "./components/frame_motion/css/styles.css"
+/*
 import { Swiper_Teste } from './components/Swipe/Teste/Teste';
 import ButtonUsage from './components/ReactUi/Teste/Teste';
-
+import ButtonToggle from './components/buttonToogle/buttonToggle';
+import FramerMotion from './components/frame_motion/framer_motion';
+*/
 function App() {
   return (
     <div className="App flex-row center">
-      <ButtonUsage />
+      <FramerMotion5 
+      className="flex-column center text_center style_template_3"
+      style={{
+        minWidth: "95vw",
+        height: "500px",  
+        marginLeft: "10px",
+        marginRight: "10px",
+        borderRadius: "30px",
+        fontSize: "2rem",
+        fontWeight: 500
+      }} />
     </div>
   );
 }
